7d51f95d2d55afa088629cfecd68bd10a49ac3e1
KhansaCelsior <khansa.iqbal@celsiortech.com>
2022-09-17 21:17:45 +0530

    updated associations

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -29,7 +29,9 @@ export default function (sequelize: Sequelize): typeof CategoryModel {
   );
 
   return CategoryModel;
+  
+  CategoryModel.belongsToMany(QuizModel, {
+    through: 'quizCategory'
+  });
 }
-// CategoryModel.belongsToMany(QuizModel, {
-//   through: 'quizCategory'
-// });
+
